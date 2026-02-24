8b690143d46a2b29c67486ab1b6cb3c325ecf012
hyunjun <y.raymond0211@gmail.com>
2026-02-24 20:42:18 +0900

    [Bronze III] Title: 직각삼각형, Time: 180 ms, Memory: 17564 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -1,53 +1,29 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // 3개의 변 입력받기
-        while(true) {
+        while (true) {
             int a = in.nextInt();
             int b = in.nextInt();
             int c = in.nextInt();
 
-            if((a==0) && (b==0) && (c==0)) break;
+            if (a == 0 && b == 0 && c == 0) break;
 
-            int[] arr = {a, b, c};
-            Arrays.sort(arr);
-            if (arr[2] * arr[2] == arr[0] * arr[0] + arr[1] * arr[1]) {
+            if (rect(a, b, c)) {
                 System.out.println("right");
-            } else
-                System.out.println("wrong");
+            } else System.out.println("wrong");
         }
     }
-}
+    public static boolean rect(int a, int b, int c) {
+        long a2 = (long)a * a;
+        long b2 = (long)b * b;
+        long c2 = (long)c * c;
 
-/*
-import java.util.Scanner;
+        if (a2 + b2 == c2) return true;
+        if (b2 + c2 == a2) return true;
+        if (c2 + a2 == b2) return true;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        // 3개의 변 입력받기
-        while(true) {
-            int a = in.nextInt();
-            int b = in.nextInt();
-            int c = in.nextInt();
-
-            if((a==0) && (b==0) && (c==0)) break;
-            String s = " ";
-            if (c * c == a * a + b * b) {
-                s = "right";
-                System.out.println(s);
-            } else {
-                s = "wrong";
-                System.out.println(s);
-            }
-        }
+        return false;
     }
 }
-
-// 3개의 변 중에서 항상 c의 길이가 길다고 해서 틀림.
-  10 6 8 와 같은 입력이 들어오면 실제로는 직각삼각형이나 wrong을 출력함.
-  따라서 배열에 저장해두고 정렬을 해서 풀면 쉬움,
-*/
