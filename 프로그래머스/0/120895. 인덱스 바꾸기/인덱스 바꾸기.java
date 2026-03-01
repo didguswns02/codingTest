b84f817bddb3126b72982e846813f7f23f810da4
hyunjun <y.raymond0211@gmail.com>
2026-03-01 18:56:48 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 1.97 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String[] arr = my_string.split("");
+        String temp = arr[num1];
+        arr[num1] = arr[num2];
+        arr[num2] = temp;
+        String answer = "";
+        for (String str : arr) {
+            answer += str;
+        }
+        return answer;
+    }
+}
