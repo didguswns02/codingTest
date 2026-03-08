79b655db4233d917047e3f2e8e066dd61239ff16
hyunjun <y.raymond0211@gmail.com>
2026-03-08 13:43:46 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.02 ms, Memory: 91.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(String[] keyinput, int[] board) {
+        int[] answer = {0, 0};
+        int maxX = board[0] / 2;
+        int maxY = board[1] / 2;
+        for (int i = 0; i < keyinput.length; i++) {
+            if (keyinput[i].equals("up") && answer[1] < maxY) {
+                answer[1] = answer[1] + 1;
+            } 
+            if (keyinput[i].equals("down") && answer[1] > -maxY) {
+                answer[1] = answer[1] - 1;
+            }
+            if (keyinput[i].equals("left") && answer[0] > -maxX) {
+                answer[0] = answer[0] - 1;
+            }
+            if (keyinput[i].equals("right") && answer[0] < maxX) {
+                answer[0] = answer[0] + 1;
+            }
+        }
+        return answer;
+    }
+}
