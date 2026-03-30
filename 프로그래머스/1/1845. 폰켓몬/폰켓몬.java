053d509ebd182c89355fbc0d1ac6c92b7296a25f
hyunjun <y.raymond0211@gmail.com>
2026-03-30 14:07:56 +0900

    [level 1] Title: 폰켓몬, Time: 5.34 ms, Memory: 92.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,16 @@
+import java.util.Set;
+import java.util.HashSet;
+
+class Solution {
+    public int solution(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i : nums) {
+            set.add(i);
+        }
+        if (set.size() > nums.length / 2) {
+            return nums.length / 2;
+        } else {
+            return set.size();
+        }
+    }
+}
